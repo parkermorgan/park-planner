49dba6e0d47246e827e94595b6dc28c1a79b9f90
parkermorgan <156543039+parkermorgan@users.noreply.github.com>
2026-01-10 09:34:37 -0700

    Initial Commit

---
+++ park-planner/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  park-planner
+//
+//  Created by Parker Morgan on 1/10/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Welcome to Park Planner!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ park-planner/park_plannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  park_plannerApp.swift
+//  park-planner
+//
+//  Created by Parker Morgan on 1/10/26.
+//
+
+import SwiftUI
+
+@main
+struct park_plannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
