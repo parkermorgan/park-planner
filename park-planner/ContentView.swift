558dc6964bbb44de6f1d47e707880031d467e5cc
parkermorgan <156543039+parkermorgan@users.noreply.github.com>
2026-01-10 09:36:24 -0700

    Changed title and added styles

--- park-planner/ContentView.swift
+++ park-planner/ContentView.swift
@@ -14,6 +14,8 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Welcome to Park Planner!")
+                .font(.title)
+                .fontWeight(.bold)
         }
         .padding()
     }
